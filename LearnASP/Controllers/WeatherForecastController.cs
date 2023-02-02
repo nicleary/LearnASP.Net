53c088ca8cc5c4887bc1e6540afa47d13b3631e2
Nick Cleary <nicholascleary01@gmail.com>
2023-02-01 20:49:53 -0500

    Updates to using .net 6.0

--- LearnASP/Controllers/WeatherForecastController.cs
+++ LearnASP/Controllers/WeatherForecastController.cs
@@ -21,9 +21,9 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 11).Select(index => new WeatherForecast
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
